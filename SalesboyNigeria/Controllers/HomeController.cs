64cbe61a82069e32aca46ca8a3f45a20a6df3079
DM <Hyedimawaha@gmail.com>
2022-10-28 16:08:09 +0100

    updated images

--- SalesboyNigeria/Controllers/HomeController.cs
+++ SalesboyNigeria/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SalesboyNigeria.setup;
 
 namespace SalesboyNigeria.Controllers
 {
     public class HomeController : Controller
     {
         private Models.salesboyEntities db = new Models.salesboyEntities();
+        
+        [CheckAuthentication]
         public ActionResult Index()
         {
             return View();
