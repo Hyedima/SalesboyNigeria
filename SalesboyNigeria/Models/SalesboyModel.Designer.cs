5951d37853240fc8601bc4b122e7751210566dd8
Hwaha <hwaha@its.jnj.com>
2023-03-06 22:56:08 +0100

    updated icon

--- SalesboyNigeria/Models/SalesboyModel.Designer.cs
+++ SalesboyNigeria/Models/SalesboyModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Hyedima\Documents\Visual Studio 2022\Projects\SalesboyNigeria\SalesboyNigeria\Models\SalesboyModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\hyedi\OneDrive\Documents\GitHub\SalesboyNigeria\SalesboyNigeria\Models\SalesboyModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
